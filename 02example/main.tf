b3515e2854192538e31054763a0f639e7c044e7d
Vahid Khoshghamatazad <vahid.azad@gmail.com>
2023-02-11 14:34:28 +0100

    changing env name

--- 02example/main.tf
+++ 02example/main.tf
@@ -5,20 +5,20 @@ provider "aws" {
 variable "vpc_cidr_block" {}
 variable "subnet_cidr_block" {}
 variable  "avail_zone" {}
-variable "env" {}
+variable "env_prefix" {}
 
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
     tags = {
-      Name    : "${var.avail_zone}-myapp-vpc"
+      Name    : "${var.env_prefix}-myapp-vpc"
     }
 }
 
 resource "aws_subnet" "myapp-subnet-1" {
-    vpc_id = aws_vpc.dev-vpc.id
+    vpc_id = aws_vpc.myapp-vpc.id
     cidr_block = var.subnet_cidr_block
     availability_zone = var.avail_zone
     tags = {
-      "Name" = "${var.avail_zone}-myapp-subnet-1"
+      "Name" = "${var.env_prefix}-myapp-subnet-1"
     }
 }
--- 02example/terraform.tfvars
+++ 02example/terraform.tfvars
@@ -1,4 +1,4 @@
 vpc_cidr_block      = "10.0.0.0/16"
 subnet_cidr_block   = "10.0.10.0/24"
 avail_zone          = "eu-west-1b"
-env                 = "dev"
+env_prefix          = "dev"
