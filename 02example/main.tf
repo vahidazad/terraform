abd6197af56f1eea588eb234e6a20bfeca2e6248
Khoshghamatazad <vahid.khoshghamatazad@deu.kyocera.com>
2023-02-12 11:38:29 +0100

    fixing the group refresh for docker

--- 02example/main.tf
+++ 02example/main.tf
@@ -107,7 +107,8 @@ resource "aws_instance" "myapp-server" {
                   #!/bin/bash
                   sudo yum update -y && sudo yum install -y docker 
                   sudo systemctl start docker
-                  sudo usermode -aG docker ec2-user
+                  sudo usermod -aG docker ec2-user
+                  newgrp docker
                   docker run -p  8080:80 nginx 
               EOF
   
